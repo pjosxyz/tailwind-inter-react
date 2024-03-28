d2fdd57126cfd3d2f4e614f392cd506c0b6b4291
pjos <pjosxyz@gmail.com>
2024-03-28 10:14:44 +0000

    tailwind with inter set up

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
